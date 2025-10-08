c5a01dcb5bf9cfbd836a8dca22263f59c317f709
James Woglom <j@wogloms.net>
2025-10-07 20:23:44 -0400

    Update 40-output.sh

--- scripts/stages/40/40-output.sh
+++ scripts/stages/40/40-output.sh
@@ -7,7 +7,7 @@ color_echo "used on rootfs: $(du -sh "$ROOTFS_PATH" | sed "s/\s.*//")" -Yellow
 echo
 
 color_echo ">> Compressed Sizes"
-color_echo "size of image: $(du -sh "$OUTPUT_PATH"/nocturne-connector_"$CONNECTOR_IMAGE_VERSION".img.gz | sed "s/\s.*//")" -Yellow
+color_echo "size of image: $(du -sh "$OUTPUT_PATH"/nocturne-connector_"$CONNECTOR_IMAGE_VERSION"_"$ALPINE_ARCH".img.gz | sed "s/\s.*//")" -Yellow
 echo
 
 color_echo "$WORK_PATH" -Yellow
